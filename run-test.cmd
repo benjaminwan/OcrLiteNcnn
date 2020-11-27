573f98f06c56fd15f4bc8c1557dbce1d5b586068
benjaminwan <benjaminwan@163.com>
2020-11-27 16:48:26 +0800

    合并build和build-lib，通过输入选项选择

--- run-test.cmd
+++ run-test.cmd
@@ -7,7 +7,7 @@ set OMP_NUM_THREADS=%NUMBER_OF_PROCESSORS%
 
 :: run Win x64
 copy opencv\build\x64\vc14\bin\opencv_world3411.dll build-win /Y
-pushd build-win
+pushd build
 OcrLiteOnnxToNcnn.exe --models ../models --image ../../test_imgs/1.jpg ^
                 --numThread $NUM_THREADS --padding 50 --imgResize 0 ^
                 --boxScoreThresh 0.6 --boxThresh 0.3 --minArea 3 ^
