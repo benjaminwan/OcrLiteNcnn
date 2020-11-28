e321b1afbb407e88614e536b7f67d5dcea647408
benjaminwan <benjaminwan@163.com>
2020-11-28 22:29:10 +0800

    修改ncnn mac 编译脚本

--- scripts/build-ncnn-mac.sh
+++ scripts/build-ncnn-mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-export VULKAN_SDK=/~/vulkansdk-macos-1.1.114.0/macOS
+#brew install libomp
+#export VULKAN_SDK=/~/vulkansdk-macos-1.1.114.0/macOS
 
 ##### MacOS
 mkdir -p build-mac
@@ -12,7 +12,7 @@ cmake -DNCNN_OPENMP=ON \
       -DOpenMP_CXX_LIB_NAMES="omp" \
       -DOpenMP_C_LIB_NAMES="omp" \
       -DOpenMP_omp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib \
-      -DNCNN_VULKAN=ON \
+      -DNCNN_VULKAN=OFF \
       ..
 make -j 8
 make install
